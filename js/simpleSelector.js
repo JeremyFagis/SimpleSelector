48e298e6727431a3ac9fde3b9ecc3ee7b41ff9ec
Jeremy Fagis <jeremy@fagis.fr>
2013-09-04 22:54:17 +0200

    Added font icon.

--- js/simpleSelector.js
+++ js/simpleSelector.js
@@ -7,17 +7,19 @@ Copyright (c) 2013 Jeremy FAGIS (http://fagis.fr)
 
 (function($) { 
     $.fn.simpleSelector = function(params) {
-        params = $.extend( {selector: 'selector', id: null}, params);
+        params = $.extend( {selector: 'selector', id: null, caretClass: 'icon-lines'}, params);
 
         this.each(function() {
             $defaultText = $(this).data('default-text') || '---'
             $wrapperId = $(this).data('wrapper-id') || ''
+            $caretClass = $(this).data('caret-class') || params.caretClass
 
             $val = $(this).find('option:selected').text();
             $val = ($val)? $val : $defaultText ;
 
             $(this).wrap('<div class="' + params.selector + '-wrapper" id="' + $wrapperId + '" />').removeClass(params.selector);
             $('<span/>').text($val).prependTo($(this).parent());
+            $('<i class="' + $caretClass + '"></i>').appendTo($(this).parent());
 
             // Add event
             $(this).on('change', function() {
